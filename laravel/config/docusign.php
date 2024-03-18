a2fbabe2616ba8308dcb9950b7954a429d07c024
arvinantigua <139552868+arvinantigua@users.noreply.github.com>
2024-03-18 15:45:02 +0800

    Add config file

---
+++ laravel/config/docusign.php
@@ -0,0 +1,12 @@
+<?php
+
+return [    
+    'base_url' => env('DOCUSIGN_BASE_URL'),
+    'oauth_url' => env('DOCUSIGN_OAUTH_URL'),
+    'oauth_base' => env('DOCUSIGN_OAUTH_BASE'),
+    'account_id' => env('DOCUSIGN_ACCOUNT_ID'),
+    'user_id' => env('DOCUSIGN_USER_ID'),
+    'client_id' => env('DOCUSIGN_CLIENT_ID'),
+    'client_secret' => env('DOCUSIGN_CLIENT_SECRET'),
+    'private_key' => env('DOCUSIGN_PRIVATE_KEY')
+];
